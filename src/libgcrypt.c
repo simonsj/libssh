259721e52314f5ff47ae3b8e551ddda7f0246116
Andreas Schneider <asn@cryptomilk.org>
2024-12-03 13:27:38 +0100

    gcrypt: Allocate 32k of secure memory
    In the meantime libgcrypt allocates 32k of secure memory, the minimum is
    16k.
    
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>

--- src/libgcrypt.c
+++ src/libgcrypt.c
@@ -966,7 +966,7 @@ int ssh_crypto_init(void)
     gcry_control (GCRYCTL_SUSPEND_SECMEM_WARN);
 
     if (!gcry_control(GCRYCTL_INITIALIZATION_FINISHED_P, 0)) {
-        gcry_control(GCRYCTL_INIT_SECMEM, 4096);
+        gcry_control(GCRYCTL_INIT_SECMEM, 32768, 0);
         gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
     }
 
