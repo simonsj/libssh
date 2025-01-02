cbcd6d6f46d16384001b310bbf3945d11df30a1f
Jakub Jelen <jjelen@redhat.com>
2025-01-02 11:18:03 +0100

    Happy new year 2025!
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/client.c
+++ src/client.c
@@ -895,7 +895,7 @@ error:
  */
 const char *ssh_copyright(void)
 {
-    return SSH_STRINGIFY(LIBSSH_VERSION) " (c) 2003-2024 "
+    return SSH_STRINGIFY(LIBSSH_VERSION) " (c) 2003-2025 "
            "Aris Adamantiadis, Andreas Schneider "
            "and libssh contributors. "
            "Distributed under the LGPL, please refer to COPYING "
